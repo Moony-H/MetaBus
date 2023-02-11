2dcaabb835bf142e2cec431024d7dbb8e08259a1
Moony-H <moonylight247@gmail.com>
2023-02-11 16:47:45 +0900

    station arrival fragment added

--- presentation/src/main/java/com/moonyh/presentation/fragment/StationArrivalFragment.kt
+++ presentation/src/main/java/com/moonyh/presentation/fragment/StationArrivalFragment.kt
@@ -1,15 +1,52 @@
 package com.moonyh.presentation.fragment
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.moonyh.presentation.adapter.StationArrivalAdapter
 import com.moonyh.presentation.databinding.FragmentStationArrivalBinding
 import com.moonyh.presentation.viewmodel.BaseViewModel
+import com.moonyh.presentation.viewmodel.MainViewModel
+import com.moonyh.presentation.viewmodel.StationArrivalViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 class StationArrivalFragment:BaseFragment<FragmentStationArrivalBinding>(){
 
     override val viewBindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentStationArrivalBinding
         get() = FragmentStationArrivalBinding::inflate
 
-    override val viewModel:
+    override val viewModel: StationArrivalViewModel by viewModels()
+    private val mainViewModel:MainViewModel by viewModels (ownerProducer = {requireActivity()})
+
+    private val adapter:StationArrivalAdapter by lazy {
+        StationArrivalAdapter{
+
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        binding.list.adapter=adapter
+        binding.list.layoutManager=LinearLayoutManager(requireContext()).apply {
+            orientation=LinearLayoutManager.VERTICAL
+        }
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.stationArrivalInfo.collectLatest {
+                adapter.submitList(it)
+            }
+        }
+
+
+
+        return binding.root
+    }
 
 }
--- presentation/src/main/java/com/moonyh/presentation/viewmodel/StationArrivalViewModel.kt
+++ presentation/src/main/java/com/moonyh/presentation/viewmodel/StationArrivalViewModel.kt
@@ -1,9 +1,17 @@
 package com.moonyh.presentation.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.viewModelScope
+import com.moonyh.domain.model.BusInfo
+import com.moonyh.domain.model.query.StationArrivalInfoQuery
 import com.moonyh.domain.usecase.GetStationArrivalInfoUseCase
+import com.moonyh.presentation.R
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +20,20 @@ class StationArrivalViewModel @Inject constructor(
     private val getStationArrivalInfoUseCase: GetStationArrivalInfoUseCase
 ) : BaseViewModel(application) {
 
+
+    private val _stationArrivalInfo= MutableStateFlow<ArrayList<out BusInfo>>(arrayListOf())
+    val stationArrivalInfo=_stationArrivalInfo.asStateFlow()
+
+    fun startGettingArrivalInfo(cityCode:String,stationId:String){
+        val query=StationArrivalInfoQuery(context.getString(com.moonyh.data.R.string.apiKey),cityCode,stationId)
+        viewModelScope.launch {
+            while(isActive){
+                runApiUseCase(getStationArrivalInfoUseCase,query,false){
+                    _stationArrivalInfo.value=it.items
+                }
+                delay(10000)
+
+            }
+        }
+    }
 }
